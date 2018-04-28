8c902d257051093addb18522ab39feda0431ed3d
陳韋宏 <33064343+TaKuKu@users.noreply.github.com>
2018-04-28 17:31:21 +0800

    Update PythonApplication3.py

--- PythonApplication3.py
+++ PythonApplication3.py
@@ -1,3 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+
+E = Image.open("E.png")
+Eprime = Image.open("Eprime.png")
+
+I = Image.open("I.png")
+
+Key1 = Image.open("key1.png")
+Key2 = Image.open("key2.png")
+
+
+E.close()
+Eprime.close()
+I.close()
+Key1.close()
+Key2.close()
