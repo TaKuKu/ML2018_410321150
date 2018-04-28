422b72423327338e9d81004769843b06592ead73
陳韋宏 <33064343+TaKuKu@users.noreply.github.com>
2018-04-28 18:20:14 +0800

    Update PythonApplication3.py

--- PythonApplication3.py
+++ PythonApplication3.py
@@ -1,23 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import random
 
 E = Image.open("E.png")
 Eprime = Image.open("Eprime.png")
-
 I = Image.open("I.png")
-
 Key1 = Image.open("key1.png")
 Key2 = Image.open("key2.png")
+MaxIterLimit = 100
 
 height = E.size[1]
 width = E.size[0]
 Epoch = 1
 w = np.random.random((1,3))
-print (w)
+
+while Epoch < MaxIterLimit or < 0.01:
+    for y in height:
+        for x in width:
+
+
+
 
 E.close()
 Eprime.close()
 I.close()
 Key1.close()
 Key2.close()
+
