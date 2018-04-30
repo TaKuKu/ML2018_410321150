938080a0977bda5df162b1ee7d58b6c2c9e24df4
陳韋宏 <33064343+TaKuKu@users.noreply.github.com>
2018-04-30 16:37:10 +0800

    Update PythonApplication3.py

--- PythonApplication3.py
+++ PythonApplication3.py
@@ -20,9 +20,9 @@ while Epoch < MaxIterLimit:
         for y in range(0, height):
             a = w[0] * Key1.getpixel((x, y)) + w[1] * Key2.getpixel((x, y)) + w[2] * I.getpixel((x, y))
             e = E.getpixel((x, y)) - a
-            w[0] += 0.00001 * e * Key1[x][y]
-            w[1] += 0.00001 * e * Key2[x][y]
-            w[2] += 0.00001 * e * I[x][y]
+            w[0] += 0.00001 * e * Key1.getpixel((x, y))
+            w[1] += 0.00001 * e * Key2.getpixel((x, y))
+            w[2] += 0.00001 * e * I.getpixel((x, y))
     Epoch += 1
 
 
